52cd021a15ee1cd3c7c58d0990ee5af06557f1f5
Dr-ZHUIM <372919916@qq.com>
2024-03-01 19:07:15 +0800

    :

--- src/app/Tools/page.tsx
+++ src/app/Tools/page.tsx
@@ -7,7 +7,6 @@ export default function ToolsPage() {
       <h1 className="px-8 mb-12">Tools</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 justify-between justify-items-stretch">
         <Reviews.Al1sReview />
-        <Reviews.PokemonReview />
       </div>
     </>
   );
