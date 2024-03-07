e3c426e90a9501e3fd6a6ec16d7dfc17fd42ae02
Dr-ZHUIM <372919916@qq.com>
2024-03-07 22:27:32 +0800

    feats:
    1. post-css-center
    
    fixed:
    
    1. fix the problem make git action cannot run 30 minutes

---
+++ src/articles/Post/post-css-center/components.tsx
@@ -0,0 +1,47 @@
+const boxStyle: React.CSSProperties = {
+  backgroundColor: "rgb(224, 206, 247)",
+  border: "5px solid rebeccapurple;",
+};
+
+const childStyle: React.CSSProperties = {
+  background: "rgba(255,255,255,.5)",
+  border: "1px solid black",
+  padding: 10,
+};
+
+export function FlexCase() {
+  const flexBoxStyle: React.CSSProperties = {
+    height: 300,
+    gap: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  return (
+    <div style={{ ...boxStyle, ...flexBoxStyle }}>
+      <div style={childStyle}>子级元素1</div>
+      <div style={childStyle}>子级元素2</div>
+    </div>
+  );
+}
+
+export function MarginCase() {
+  const _boxStyle: React.CSSProperties = {
+    width: "100%",
+    height: 300,
+  };
+
+  const marginBoxStyle: React.CSSProperties = {
+    width: "fit-content",
+    marginInline: "auto",
+    marginBlock: 10,
+  };
+  return (
+    <div style={{ ...boxStyle, ..._boxStyle }}>
+      <div style={{ ...childStyle, ...marginBoxStyle }}>子级元素1</div>
+      <div style={{ ...childStyle, ...marginBoxStyle }}>子级元素2</div>
+    </div>
+  );
+}
+
